43558120c658b60bc55a94defaf865a21ea77591
LeoDarcy <baidurnt@163.com>
2019-03-21 21:19:52 +0800

    LOC function 1.0

---
+++ LOC/Main.java
@@ -0,0 +1,50 @@
+package org.dstadler.jgit.MyJGit;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+
+
+    public static void main(String args[]) {
+        String GitAddress = "https://github.com/vegetable-father-son/Inverview-Preparation.git";
+        try {
+            GetRepository getRepository = new GetRepository();
+            Git git = getRepository.openJGitRepository(GitAddress);
+            if (git == null)
+                return;
+            System.out.println("Ready to calculate");
+            CalculateLOC calculateLOC = new CalculateLOC();
+            calculateLOC.WorkOutLOC(git);
+
+            System.out.println("Ready to cnceal");
+            getRepository.CloseJGitRepository();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        /*String gitFilePath = "D:\\课程学习\\大三上\\javasub\\java-2018f-homework";
+        File root = new File(gitFilePath);
+        Git git = null;
+        Repository repository;
+        try{
+            git = Git.open(root);
+            repository = git.getRepository();
+            CalculateLOC calculateLOC = new CalculateLOC();
+            calculateLOC.WorkOutLOC(git);
+            //return repository;
+        }catch (Exception e){
+            e.printStackTrace();
+        }*/
+        //开始计算LOC
+
+
+
+
+        //getRepository.CloseJGitRepository();
+        //Repository repository = GetResponsibilty.openReposity();
+    }
+}
